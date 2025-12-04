c65a94065c7cfcecbd6e8436a63886042059dd4d
Ginger <ginger@gingershaped.computer>
2025-12-04 09:33:29 -0500

    Remove comment left over from template

--- src/main/kotlin/computer/gingershaped/casks/Casks.kt
+++ src/main/kotlin/computer/gingershaped/casks/Casks.kt
@@ -3,13 +3,6 @@ package computer.gingershaped.casks
 import net.neoforged.fml.common.Mod
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
-/**
- * Main mod class. Should be an `object` declaration annotated with `@Mod`.
- * The modid should be declared in this object and should match the modId entry
- * in neoforge.mods.toml.
- *
- * An example for blocks is in the `blocks` package of this mod.
- */
 @Mod(Casks.ID)
 object Casks {
     const val ID = "casks"
